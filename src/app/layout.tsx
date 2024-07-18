02559e8197703ac1fd2a6145d9e97fe1ce99bc91
Leandro Licata <leandrolicata1@gmail.com>
2024-07-18 14:29:58 -0300

    ..

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Exo_2 } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/header";
+import Header from "@/components/Header";
 
 
 
